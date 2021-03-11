0793323170e86f837c11b78f65b3c7e8533acdef
cristianabote <cristianamihaelaplesa@gmail.com>
2021-03-11 11:11:58 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Pozitia caracterului "." dintr-un sir dat
+        String text="training.ro";
+        int pozitie=text.indexOf(".");
+        System.out.println("Caracterul . se afla pe pozitia numarul "+ pozitie+" din sirul dat:  training.ro");
+    }
+}
